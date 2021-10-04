a6300808816392f01bd69306d11e0b1f0557eff4
sadegh <sadeghhanifehnia@gmail.com>
2021-10-04 22:21:34 +0330

    add maximum for expire time

--- shorter/models.py
+++ shorter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from .enums import StatusTypes
 from .utils import create_random_string, create_random_password
 from django.utils import timezone
+from django.core.validators import MaxValueValidator
 
 
 class Shorter(models.Model):
@@ -12,7 +13,7 @@ class Shorter(models.Model):
     short_url = models.CharField(max_length=15, unique=True)
     counter = models.PositiveIntegerField(default=0)
     created_time = models.DateTimeField(auto_now_add=True)
-    validate_time = models.PositiveIntegerField(default=60)  # minutes add for expire time
+    validate_time = models.PositiveIntegerField(default=60, validators=[MaxValueValidator(1440)])  # minutes add for expire time maximum 1 day
     expire_time = models.DateTimeField()
     status = models.CharField(choices=StatusTypes.choices, default=StatusTypes.public, max_length=15)
     one_time_password = models.CharField(max_length=12)

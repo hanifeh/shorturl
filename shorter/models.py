c7aef168ab1467eda5f52553db566fe12f08897f
sadegh <sadeghhanifehnia@gmail.com>
2021-10-05 14:54:01 +0330

    remove expire time from model and add serializer for shorter model

--- shorter/models.py
+++ shorter/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from .enums import StatusTypes
 from .utils import create_random_string, create_random_password
-from django.utils import timezone
 from django.core.validators import MaxValueValidator
 
 
@@ -14,20 +13,16 @@ class Shorter(models.Model):
     counter = models.PositiveIntegerField(default=0)
     created_time = models.DateTimeField(auto_now_add=True)
     validate_time = models.PositiveIntegerField(default=60, validators=[MaxValueValidator(1440)])  # minutes add for expire time maximum 1 day
-    expire_time = models.DateTimeField()
     status = models.CharField(choices=StatusTypes.choices, default=StatusTypes.public, max_length=15)
     one_time_password = models.CharField(max_length=12)
 
     def save(self, *args, **kwargs):
         """
         create short url
-        add expire time
         create random password if status is privet
         """
         if not self.short_url:
             self.short_url = self.create_short_url()
-        if not self.expire_time:
-            self.expire_time = timezone.now() + timezone.timedelta(minutes=self.validate_time)
         if self.status == 'privet' and not self.one_time_password:
             self.one_time_password = create_random_password()
         super().save(*args, **kwargs)
---
+++ shorter/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from . import models
+from django.utils.timezone import timedelta
+from .enums import StatusTypes
+
+
+class ShorterSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for Shorter
+    """
+    short_url = serializers.CharField(required=False, max_length=15)
+    validate_time = serializers.IntegerField(required=False, min_value=5, max_value=1440)
+    status = serializers.CharField(required=False, max_length=12)
+
+    class Meta:
+        model = models.Shorter
+        fields = (
+            'original_url',
+            'short_url',
+            'validate_time',
+            'status',
+        )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['expire_time'] = instance.created_time + timedelta(minutes=representation.pop('validate_time'))
+        if representation['status'] == 'privet':
+            representation['one_time_password'] = instance.one_time_password
+        return representation
+
+    def validate(self, attrs):
+        status = attrs.get('status')
+        if status and status not in StatusTypes.values:
+            raise serializers.ValidationError('status must be public or privet.')
+        return attrs
